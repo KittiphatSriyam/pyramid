b89103207329296aa938eff34e906eaeae8a46a9
KITTIPHAT SRIYAM <31922824+KittiphatSriyam@users.noreply.github.com>
2020-01-26 20:39:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,4 +14,20 @@ function pyramid(n) {
     console.log(level);
   }
 }
+
+function pyramid2(n) {
+
+  for(let i=1; i<= n; i++){
+
+    let str = ' '.repeat(n-i);
+
+    let str2 = '*'. repeat(i*2 -1)
+
+ 
+
+    console.log(str + str2 + str);
+
+  }
+
+}
 pyramid(75);
